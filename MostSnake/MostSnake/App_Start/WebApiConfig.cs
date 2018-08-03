a23425a21e07e37ae9282e751f6a9b073c73182b
Richard <richardhovenkamp@hotmail.com>
2018-08-03 21:44:35 +0200

    Basic API setup

---
+++ MostSnake/MostSnake/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace MostSnake
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            config.Routes.MapHttpRoute(
+                name: "MostSnake",
+                routeTemplate: "{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MostSnake/MostSnake/Controllers/MostSnakeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MostSnake.Controllers
+{
+    public class MostSnakeController : ApiController
+    {
+        public IHttpActionResult GetMostSnakeResult()
+        {
+            string result = "Some freaky result";
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetMostSnakeResultTest()
+        {
+            string result = "Some different result";
+            return Ok(result);
+        }
+    }
+}
